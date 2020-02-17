83d837cc5d78df573dde236bf399b530eecd6363
osipov180 <osipov180@gmail.com>
2020-02-17 21:07:54 +0200

    5 tests is ready

--- tests.py
+++ tests.py
@@ -1,2 +1,43 @@
 import unittest
+from main import get_number_from_index, get_empty_list
 
+class Test_2048(unittest.TestCase):
+
+    def test_1(self):
+        self.assertEqual(get_number_from_index(1,2),7)
+
+    def test_2(self):
+        self.assertEqual(get_number_from_index(3,3),16)
+
+    def test_3(self):
+        a = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16]
+        mas = [
+            [0, 0, 0, 0],
+            [0, 0, 0, 0],
+            [0, 0, 0, 0],
+            [0, 0, 0, 0]
+        ]
+        self.assertEqual(get_empty_list(mas),a)
+
+    def test_4(self):
+        a = [5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16]
+        mas = [
+            [1, 1, 1, 1],
+            [0, 0, 0, 0],
+            [0, 0, 0, 0],
+            [0, 0, 0, 0]
+        ]
+        self.assertEqual(get_empty_list(mas),a)
+
+    def test_5(self):
+        a = [ ]
+        mas = [
+            [1, 1, 1, 1],
+            [1, 1, 1, 1],
+            [1, 1, 1, 1],
+            [1, 1, 1, 1]
+        ]
+        self.assertEqual(get_empty_list(mas),a)
+
+if __name__ == 'main':
+    unittest.main()
