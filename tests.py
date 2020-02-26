2dc0b371dc0bbf44ca9533613362c7db6bbc77df
osipov180 <osipov180@gmail.com>
2020-02-26 21:06:52 +0200

    Draw rectangles on fild complete

--- tests.py
+++ tests.py
@@ -1,5 +1,6 @@
 import unittest
-from main import get_number_from_index, get_empty_list
+from logics import get_number_from_index, get_empty_list, get_index_from_number, \
+    is_zero_in_mas
 
 class Test_2048(unittest.TestCase):
 
@@ -38,6 +39,42 @@ class Test_2048(unittest.TestCase):
             [1, 1, 1, 1]
         ]
         self.assertEqual(get_empty_list(mas),a)
+
+    def test_6(self):
+        self.assertEqual(get_index_from_number(7),(1,2))
+
+    def test_7(self):
+        self.assertEqual(get_index_from_number(16),(3,3))
+
+    def test_8(self):
+        self.assertEqual(get_index_from_number(1),(0,0))
+
+    def test_9(self):
+        mas = [
+            [1, 1, 1, 1],
+            [1, 1, 1, 1],
+            [1, 1, 1, 1],
+            [1, 1, 1, 1],
+        ]
+        self.assertEqual(is_zero_in_mas(mas), False)
+
+    def test_10(self):
+        mas = [
+            [1, 1, 1, 1],
+            [1, 1, 0, 1],
+            [1, 1, 1, 1],
+            [1, 1, 1, 1]
+        ]
+        self.assertEqual(is_zero_in_mas(mas),True)
+
+    def test_11(self):
+        mas = [
+            [0, 1, 1, 1],
+            [1, 0, 1, 1],
+            [1, 1, 1, 1],
+            [1, 0, 1, 0]
+        ]
+        self.assertEqual(is_zero_in_mas(mas), True)
 
 if __name__ == 'main':
     unittest.main()
